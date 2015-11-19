6d49186ec63b47f3bcd3128d539319d0c78ff4b7
Reece Small <rsmall@salisbury6c.ac.uk>
2015-11-19 14:54:00 +0000

    Update pir.py

--- pir.py
+++ pir.py
@@ -1,4 +1,22 @@
 import RPi.GPIO as gpio
 import time, sys
 
-c
+gpio.setmode(gpio.BOARD)
+pirPin = 16
+gpio.setup(pirPin, gpio.IN)
+
+def motion(pirPin):
+  print('motion detected')
+
+print('PIR test, ctrl + c to exit')
+time.sleep(1)
+print('Ready')
+
+try:
+  gpio.add_event_detect(pirPin,gpio.RISING,callback=MOTION)
+  while:
+    time.sleep(10)
+except KeyboardInterrupt:
+  print('Quit')
+  gpio.cleanup()
+  sys.exit()
