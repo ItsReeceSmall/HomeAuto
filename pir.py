3ca46d8fe0c5cf12ca45805d1f0226b4c0b8b184
Reece Small <rsmall@salisbury6c.ac.uk>
2015-11-19 15:12:54 +0000

    Update pir.py

--- pir.py
+++ pir.py
@@ -1,22 +1,32 @@
 import RPi.GPIO as gpio
 import time, sys
 
+# Methods
+def getPir():
+  print('Checking for PIR activity. . .')
+  i = gpio.input(16)
+  if i == 0:
+    print('- Activity detected')
+    print('- Lights ON')
+    #lights code here
+  if i == 1:
+    print('- Activity Undetected')
+    print('- Lights OFF')
+    #lights code here
+  time.sleep(0.1)
+
+def main():
+  getPir()
+
 gpio.setmode(gpio.BOARD)
+
+# Pins
 pirPin = 16
+
+# Pin Setup
 gpio.setup(pirPin, gpio.IN)
 
-def motion(pirPin):
-  print('motion detected')
-  time.sleep(1)
-
-while True:
-    i = gpio.input(16)
-    if i == 0:
-        print('No Intruders' + str(i))
-        time.sleep(0.1)
-    if i == 1:
-        print('Intruder Detected' + str(i))
-        time.sleep(0.1)
+main()
 
 gpio.cleanup()
 sys.exit()
