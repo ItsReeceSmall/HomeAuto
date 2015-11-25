6a3aec13a119055304e198b0464e0dea0103acfb
Reece Small <rsmall@salisbury6c.ac.uk>
2015-11-25 11:09:41 +0000

    Update pir.py

--- pir.py
+++ pir.py
@@ -1,13 +1,13 @@
 import RPi.GPIO as gpio
 import time, sys, os, glob
-
+'''
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
 device_folder = golb.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1-slave'
-
+'''
 # Methods
 def getPir():
   print('Checking for PIR activity. . .')
@@ -26,18 +26,44 @@ def getPir():
 
 #def getTemp():
 
-#def getDist():
+def getDist(TRIG, ECHO):
+    #This is a pre written method which checks an ultrasonic sensor that is connected to the pi
+    #So the pins will be imported depending which sensor is running in the while loop further down
+    gpio.setup(TRIG,gpio.OUT)
+    gpio.setup(ECHO,gpio.IN)
+    gpio.output(TRIG, False)
+    #print "Waiting For Sensor To Settle"
+    time.sleep(0.2)
+    gpio.output(TRIG, True)
+    time.sleep(0.00001)
+    gpio.output(TRIG, False)
+    while gpio.input(ECHO)==0:
+      pulse_start = time.time()
+    while gpio.input(ECHO)==1:
+      pulse_end = time.time()
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+    #print ("Distance: " + str(distance) + "cm")
+    return distance
 
 def main():
   getPir()
   #getTemp()
-  #getDist()
+  outDist = getDist(trigL, echoL)
+  print (outDist)
+  inDist = getDist(trigR, echoR)
+  print (inDist)
   
 gpio.setmode(gpio.BOARD)
 
 # the Pins
 pirPin = 16
 pirLight = 18
+trigL = 38
+echoL = 36
+trigR = 37
+echoR = 35
 
 # Pin Setup
 gpio.setup(pirPin, gpio.IN)
