7bc9283e27517db233b219dc65a30a65a8fe6bca
Reece Small <rsmall@salisbury6c.ac.uk>
2015-12-03 15:05:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as gpio
 import time, sys, os, glob, threading, datetime
 import pir
+#import dist
+#import temp
 '''
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -28,35 +30,12 @@ def main():
 
 def program(pirPin, pirLight, trigL, echoL, trigR, echoR):
   threading.Timer(1, pir.getPir(pirPin, pirLight)).start()
-  outDist = getDist(trigL, echoL)
+  outDist = dist.getDist(trigL, echoL)
   print (outDist)
   time.sleep(1)
-  inDist = getDist(trigR, echoR)
+  inDist = dist.getDist(trigR, echoR)
   print (inDist)
-
-#def getTemp():
-
-def getDist(TRIG, ECHO):
-    #This is a pre written method which checks an ultrasonic sensor that is connected to the pi
-    #So the pins will be imported depending which sensor is running in the while loop further down
-    gpio.setup(TRIG,gpio.OUT)
-    gpio.setup(ECHO,gpio.IN)
-    gpio.output(TRIG, False)
-    #print "Waiting For Sensor To Settle"
-    time.sleep(0.2)
-    gpio.output(TRIG, True)
-    time.sleep(0.00001)
-    gpio.output(TRIG, False)
-    while gpio.input(ECHO)==0:
-      pulse_start = time.time()
-    while gpio.input(ECHO)==1:
-      pulse_end = time.time()
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150
-    distance = round(distance, 2)
-    #print ("Distance: " + str(distance) + "cm")
-    return distance
-
+  
 main()
 
 gpio.cleanup()
