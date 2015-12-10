a8442bfa4392953d48ae0cca77a3b316509e3bb2
Reece Small <rsmall@salisbury6c.ac.uk>
2015-12-10 15:32:01 +0000

    Update pir.py

--- pir.py
+++ pir.py
@@ -5,12 +5,12 @@ def getPir(pirState, pirPin, pirLight):
   
   print('Checking for PIR activity. . .')
   if pirState == 1:
-    tiem.sleep(10)
-  i = gpio.input(pirPin)
-  if i == 0:
+    time.sleep(10)
+  pirState = gpio.input(pirPin)
+  if pirState == 0:
     print('- Activity Undetected: Lights OFF')
     gpio.output(pirLight, gpio.HIGH) # Turns off light
-  if i == 1:
+  if pirState == 1:
     print('- Activity Detected: Lights ON')
     gpio.output(pirLight, gpio.LOW) # Turns on light
-  return i
+  return pirState
