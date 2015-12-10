bcdae60d92054f2d26905ce99270d7405488d2a8
Reece Small <rsmall@salisbury6c.ac.uk>
2015-12-10 15:26:13 +0000

    Update pir.py

--- pir.py
+++ pir.py
@@ -1,13 +1,16 @@
 import RPi.GPIO as gpio
 import time, sys, os, glob, threading, datetime
 
-def getPir(pirPin, pirLight):
+def getPir(pirState, pirPin, pirLight):
+  
   print('Checking for PIR activity. . .')
+  if pirState = 1:
+    tiem.sleep(10)
   i = gpio.input(pirPin)
   if i == 0:
     print('- Activity Undetected: Lights OFF')
     gpio.output(pirLight, gpio.HIGH) # Turns off light
   if i == 1:
     print('- Activity Detected: Lights ON')
-    gpio.output(pirLight, gpio.LOW)
-    time.sleep(10)# Turns on light
+    gpio.output(pirLight, gpio.LOW) # Turns on light
+  return i
