aeb37819a6220ca8698e562dbf70130352b35d9b
Reece Small <rsmall@salisbury6c.ac.uk>
2015-12-10 14:57:47 +0000

    Update pir.py

--- pir.py
+++ pir.py
@@ -9,5 +9,5 @@ def getPir(pirPin, pirLight):
     gpio.output(pirLight, gpio.HIGH) # Turns off light
   if i == 1:
     print('- Activity Detected: Lights ON')
-    threading.Timer(5, gpio.output(pirLight, gpio.LOW)).start()
-    #gpio.output(pirLight, gpio.LOW) # Turns on light
+    gpio.output(pirLight, gpio.LOW)
+    time.sleep(10)# Turns on light
