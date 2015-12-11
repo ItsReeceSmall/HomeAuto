fc75a6d3be07b7b0ce14bd931da24ad22ae63931
Reece Small <rsmall@salisbury6c.ac.uk>
2015-12-11 12:06:47 +0000

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,7 +4,7 @@ import time, sys, os, glob, threading, datetime
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-temp_sensor = ‘sys/bus/w1/devices/28-000005e2fdc3/w1_slave’
+temp_sensor = (‘sys/bus/w1/devices/28-000005e2fdc3/w1_slave’)
 
 def temp_raw():
   f = open(temp_sensor, 'r')
