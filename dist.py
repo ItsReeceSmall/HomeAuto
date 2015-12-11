5950e92df558bbc2f498659ddb89eba7d5a9990b
Reece Small <rsmall@salisbury6c.ac.uk>
2015-12-11 09:15:44 +0000

    Update dist.py

--- dist.py
+++ dist.py
@@ -19,5 +19,5 @@ def getDist(TRIG, ECHO):
     pulse_duration = pulse_end - pulse_start
     distance = pulse_duration * 17150
     distance = round(distance, 2)
-    #print ("Distance: " + str(distance) + "cm")
+    print ("Distance: " + str(distance) + "cm")
     return distance
